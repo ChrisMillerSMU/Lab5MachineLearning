ee0acd0cd071e12f531e1b104b9af574c644df85
Ethan <ehaugen@smu.edu>
2023-11-18 14:50:16 -0600

    Initial UI changes

--- xcode/Saxophone ML/Saxophone ML/ViewController.swift
+++ xcode/Saxophone ML/Saxophone ML/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func switchModels(_ sender: UISegmentedControl) {
+    }
+    
+    @IBOutlet weak var modelSegmentedSwitch: UISegmentedControl!
+    
+    @IBOutlet weak var trainingLabel: UILabel!
+    
+    @IBOutlet weak var nameDropdownButton: UIButton!
+    
+    @IBAction func switchNames(_ sender: UIButton) {
+    }
+    
+    @IBAction func trainButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func testButtonPressed(_ sender: UIButton) {
+    }
+    @IBOutlet weak var predictionLabel: UILabel!
+    
+    
+    
 }
 
